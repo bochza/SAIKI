1c4a1952531996cb8e94a39738727b4554d1eeb5
bochza <sawailinares@gmail.com>
2024-04-23 10:58:47 +0200

    jiji

---
+++ SAIKI/src/snippet/Main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package snippet;
+
+/**
+ * 
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ SAIKI/src/snippet/Pacientes.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+package snippet;
+
+/**
+ * 
+ */
+public class Pacientes {
+
+}
